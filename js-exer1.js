bac8d981a035686dc0f35297ac900998ed283177
Rebecca <rebecca@chakradesign.co>
2020-03-24 14:52:50 +0530

    sextion-3

---
+++ js-exer1.js
@@ -0,0 +1,5 @@
+var firstname=prompt("what is your first name");
+var lastname=prompt("what is your last name");
+var age=prompt("what is your age");
+console.log("Your full name is" + firstname + lastname);
+console.log("you are" + age + "years old");

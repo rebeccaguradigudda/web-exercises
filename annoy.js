5741dca0f8e52ed71a639d2e40e760e2c3b59d52
Rebecca <rebecca@chakradesign.co>
2020-03-24 17:26:48 +0530

    exercises

---
+++ annoy.js
@@ -0,0 +1,16 @@
+// var answer= prompt("are we there yet");
+
+// while(answer !== "yes"){
+//     var answer = prompt("are we there yet");
+// }
+// alert("yay,we mad it");
+
+
+
+// version2
+var answer= prompt("are we there yet");
+
+while(answer.indexOf("yes")== -1){
+    var answer = prompt("are we there yet");
+}
+alert("yay,we mad it");

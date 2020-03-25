a84f15644f67684e1c6a69c59979a67378107b12
Rebecca <rebecca@chakradesign.co>
2020-03-25 13:32:33 +0530

    exercises

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var nums=[32,20,12,52];
+undefined
+var colors = ["orange","green","blue"];
+undefined
+
+colors.pop();
+"blue"
+
+colors
+(2) ["orange", "green"]
+
+colors.push("yellow");
+3
+
+colors
+(3) ["orange", "green", "yellow"]
+
+colors.indexOf("yellow");
+2
+
+colors.indexOf("indigo");
+-1
+
+var subjects = ["maths","science","english","sanskrit"];
+undefined
+
+var fav = subjects.slice(1,2);
+undefined
+
+fav
+["science"]
+
+var sub = subjects.slice();
+undefined
+sub
+(4) ["maths", "science", "english", "sanskrit"]
